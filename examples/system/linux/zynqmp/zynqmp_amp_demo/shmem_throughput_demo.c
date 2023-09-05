00fd771adc7adaed664ed6c5bc3d48d25856fe5c
Levinsky, Ben <ben.levinsky@amd.com>
2023-09-05 08:13:32 -1200

    examples: linux: zynqmp: Remove MB definition
    There is compilation error of the Linux ZynqMP AMP Demo in
    shmem_throughput_demo.c due to the MB definition being re-defined.
    
    The MB symbol is provided in lib/utilities.h as part of
    commit 7643ddae6ec8 ("lib: utilities: Match upstream for MB/GB macros")
    
    Signed-off-by: Ben Levinsky <ben.levinsky@amd.com>
    Acked-by: Sergei Korneichuk <sergei.korneichuk@amd.com>

--- examples/system/linux/zynqmp/zynqmp_amp_demo/shmem_throughput_demo.c
+++ examples/system/linux/zynqmp/zynqmp_amp_demo/shmem_throughput_demo.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2017, Xilinx Inc. and Contributors. All rights reserved.
- * Copyright (C) 2022, Advanced Micro Devices, Inc.
+ * Copyright (C) 2022-2023, Advanced Micro Devices, Inc.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -72,8 +72,6 @@
 #define PKG_SIZE_MAX 1024
 #define PKG_SIZE_MIN 16
 #define TOTAL_DATA_SIZE (1024 * 4096)
-
-#define MB (1024 * 1024) /* Mega Bytes */
 
 struct channel_s {
 	struct metal_device *shm_dev; /* Shared memory metal device */
