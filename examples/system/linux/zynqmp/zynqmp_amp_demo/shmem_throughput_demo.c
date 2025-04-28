4d06cc4f4a828fd59dfc2347ee09b1d6f66e49cb
Kangmin Lee <kangmin@rtst.co.kr>
2025-04-28 15:36:41 +0900

    kick_ipi first

--- examples/system/linux/zynqmp/zynqmp_amp_demo/shmem_throughput_demo.c
+++ examples/system/linux/zynqmp/zynqmp_amp_demo/shmem_throughput_demo.c
@@ -256,8 +256,9 @@ static int measure_shmem_throughput(struct channel_s* ch)
 					tx_count);
 			/* Kick IPI to notify RPU data is ready in
 			 * the shared memory */
-            kick_ipi(NULL);
+            //kick_ipi(NULL);
 		}
+        kick_ipi(NULL);
 		/* Stop RPU TTC counter */
 		stop_timer(ch->ttc_io, TTC_CNT_APU_TO_RPU);
 		/* Wait for RPU to signal RPU RX TTC counter is ready to
