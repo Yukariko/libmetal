b59ac36dac8dfc16f85c059108e17e279527be24
Kangmin Lee <kangmin@rtst.co.kr>
2025-05-14 19:48:30 +0900

    make ringbuffer

--- examples/system/linux/zynqmp/zynqmp_amp_demo/shmem_throughput_demo.c
+++ examples/system/linux/zynqmp/zynqmp_amp_demo/shmem_throughput_demo.c
@@ -288,7 +288,8 @@ static int measure_shmem_throughput(struct channel_s* ch)
 		uint16_t tail = rx_ring->tail % RX_RING_SIZE;
 		uint16_t size = rx_ring->buf[tail].size;
 		//LPRINTF("Starting shared mem throughput demo %hd %hd\n", tail, size);
-		memcpy(lbuf, rx_ring->buf[tail].data, size);
+		*(__uint128_t *)lbuf = *(__uint128_t *)rx_ring->buf[tail].data;
+		//memcpy(lbuf, rx_ring->buf[tail].data, size);
 		//for (i=0; i < size; i++) {
 		//	lbuf[i] = rx_ring->buf[tail].data[i];
 		//}
